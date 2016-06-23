22879cc55357f9f9dcae33b00e77524ca060d439
ChefDK User <chef@ip-10-180-247-177.ec2.internal>
2016-06-23 11:06:16 +0000

    adde verifier at  test/integration/default/serverspec/default_spec.rb

--- workstation/test/integration/default/serverspec/default_spec.rb
+++ workstation/test/integration/default/serverspec/default_spec.rb
@@ -3,7 +3,10 @@ require 'spec_helper'
 describe 'workstation::default' do
   # Serverspec examples can be found at
   # http://serverspec.org/resource_types.html
-  it 'does something' do
-    skip 'Replace this with meaningful tests'
+  describe package ('tree') do
+    if { should be_installed }
   end
+#  describe package ('cowsay') do
+#    if { should be_installed }
+#  end
 end
